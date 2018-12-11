743f675d71b6e95053c03356d6ec3797b43452fc
Umair Abdul Qadir <umair.abdulq@gmail.com>
2018-12-11 22:25:19 +0000

    Adding jQuery character counting functionality.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+  $(".new-tweet textarea").on("input", function(e) {
+    var counter = $(this).siblings(".counter");
+    var length = $(this).val().length;
+    counter.text(140 - length);
+    if (length >= 140) {
+      counter.css("color", "#FF0000");
+    } else {
+      counter.css("color", "#000000");
+    }
+  });
+});
